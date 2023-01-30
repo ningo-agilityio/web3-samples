43d43c43194dde84fe3236363edef8e6dd16f36a
Ni Ngo <ni.ngo@asnet.com.vn>
2023-01-30 16:57:57 +0700

    Arrange folders

--- practice/deploy.js
+++ practice/deploy.js
@@ -17,7 +17,7 @@ const deploy = async () => {
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    .send({ gas: '0.1', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
