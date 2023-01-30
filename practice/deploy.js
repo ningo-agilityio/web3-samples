2d338dce588c99f51f95c98e2c16df089c1536a7
Ni Ngo <ni.ngo@asnet.com.vn>
2023-01-30 17:18:53 +0700

    Remove unused code

--- practice/deploy.js
+++ practice/deploy.js
@@ -1,7 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
-
 const provider = new HDWalletProvider(
   'inject question bulb innocent draft detail radio curious shrug stamp census coffee',
   // remember to change this to your own phrase!
@@ -12,9 +11,6 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-
-  console.log('Attempting to deploy from account', accounts[0]);
-
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ gas: '0.1', from: accounts[0] });
