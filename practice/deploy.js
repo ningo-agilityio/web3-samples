e0a1f71eb670a89dd7629c13138195d4f845534e
Ni Ngo <ni.ngo@asnet.com.vn>
2023-01-31 15:29:58 +0700

    Update code to transfer from correct account

--- practice/deploy.js
+++ practice/deploy.js
@@ -15,15 +15,14 @@ const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  const currentBalance = await web3.eth.getBalance(accounts[0]);
-  console.log('Address: ', accounts[0])
+  const currentBalance = await web3.eth.getBalance(accounts[1]);
+  console.log('Address: ', accounts[1])
   console.log('Balance: ', currentBalance)
   const result = await new web3.eth.Contract(JSON.parse(interface), { _initialAmount: 0.1 })
     .deploy({ data: bytecode,  arguments: [1] })
     .send({ 
-      gas: 1, 
-      from: "0x890Ecf9DF98C9aEa625245Dad42E7671a27C4367",
-      estimateGas: 1
+      gas: 1500000, 
+      from: accounts[1],
     });
 
   console.log('Contract deployed to', result.options.address);
