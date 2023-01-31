eae8b14b92b4522dc1fb3b25abee07bf12f5c361
Ni Ngo <ni.ngo@asnet.com.vn>
2023-01-31 10:38:28 +0700

    Add env configuration

--- practice/deploy.js
+++ practice/deploy.js
@@ -1,16 +1,23 @@
+// Load env
+const { config } = require("dotenv");
+config();
+
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 const provider = new HDWalletProvider(
-  'inject question bulb innocent draft detail radio curious shrug stamp census coffee',
+  process.env.OWN_PHRASE,
   // remember to change this to your own phrase!
-  'https://goerli.infura.io/v3/4049b0af4b4b443d87eb212a432bec5b'
+  process.env.NETWORK_ENDPOINT
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
+  const currentBalance = await web3.eth.getBalance(accounts[0]);
+  console.log('Address: ', accounts[0])
+  console.log('Balance: ', currentBalance)
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ gas: '0.1', from: accounts[0] });
